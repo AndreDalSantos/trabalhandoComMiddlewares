8683865edaa9546e5199cde984cf1e75ff2f8b09
Andre Dal Santos <andredalsantos@gmail.com>
2021-04-26 18:49:44 -0300

    desafio concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if(user.todos.length >= 10 && !user.pro){
+    return response.status(403).json({ error: 'All free to dos already used.'});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.'});
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({ error: 'Id not uuid type'});
+  }
+
+//   public boolean isUUID(String s){
+//     return s.match("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+// }
+
+  const todo = user.todos.find(item => item.id === id)
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Invalid todo'});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
